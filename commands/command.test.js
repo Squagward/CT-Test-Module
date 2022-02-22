aaab78585dda59f980c140e57f9cf6cf009844a5
Cam Walter <cam.n.walter@gmail.com>
2022-02-21 20:22:42 -0600

    Command: Add tests

--- commands/command.test.js
+++ commands/command.test.js
@@ -1,11 +1,26 @@
-register("command", () => {
-    ChatLib.chat(`Test ${test++} passed!`);
-}).setName("cttest");
+/**
+ * Should give a command with 3 tab completion options, with an alias
+ */
+register("command", (subcommand) => {
+    ChatLib.chat(`Calling /cttest ${subcommand}`);
+})
+    .setTabCompletions("sub1", "sub2", "sub3")
+    .setName("cttest")
+    .setAliases("ctt");
 
+/**
+ * Should error since ct command is already taken
+ */
 try {
-    // Should error since ct command is already taken
     register("command", () => {}).setName("ct");
-    throw new Error("This should be unreachable");
+    throw new Error("This should be unreachable!");
 } catch (e) {
-    ChatLib.chat(`Test ${test++} passed!`);
+    ChatLib.chat(`Error: ${e.message}`);
 }
+
+/**
+ * Should override the ct command since overrideExisting is trues
+ */
+register("command", () => {
+    ChatLib.chat("This should be overridden!");
+}).setName("chattriggers", true);
