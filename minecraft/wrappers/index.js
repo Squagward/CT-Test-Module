85a436785099e0af658d3c3bef3f98b1329007af
Cam Walter <cam.n.walter@gmail.com>
2022-02-22 21:36:42 -0600

    Add missing files
    oops

---
+++ minecraft/wrappers/index.js
@@ -0,0 +1,10 @@
+export * from "./objects";
+
+export * from "./client.test";
+export * from "./cps.test";
+export * from "./player.test";
+export * from "./scoreboard.test";
+export * from "./server.test";
+export * from "./settings.test";
+export * from "./tablist.test";
+export * from "./world.test";
--- minecraft/wrappers/objects/inventory/index.js
+++ minecraft/wrappers/objects/inventory/index.js
@@ -1,2 +1,5 @@
 export * from "./action";
 export * from "./nbt";
+
+export * from "./inventory.test";
+export * from "./item.test";
